6242fb23a2f4145563d0041eb1e6578817378f10
fi3te <fi3te@users.noreply.github.com>
2022-05-29 15:49:30 +0200

    Updated dependencies, refactoring

---
+++ src/sensor.ts
@@ -0,0 +1,22 @@
+import * as os from "os";
+
+let sensor;
+if (os.type().toLowerCase() === 'linux') {
+    sensor = require('node-dht-sensor');
+    if (!sensor) {
+        throw new Error('Module "node-dht-sensor" not found!');
+    }
+}
+
+const SENSOR_TYPE = 22;
+const PIN = 2;
+
+function sensorRead(callback: (err: any, temperature: number, humidity: number) => void): void {
+    sensor.read(SENSOR_TYPE, PIN, callback);
+}
+
+function dummyRead(callback: (err: any, temperature: number, humidity: number) => void): void {
+    callback(undefined, 20, 50);
+}
+
+export const read = sensor ? sensorRead : dummyRead;
